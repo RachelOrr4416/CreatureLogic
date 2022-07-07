8be458caa1ff5b7ef349a968c9ed2cc415cf6cfc
Rachel Orr <orrmic@gmail.com>
2022-07-07 16:21:39 -0400

    Add project files.

---
+++ Creature.cs
@@ -0,0 +1,116 @@
+﻿using System;
+
+namespace Creature
+{
+    public class Creature
+    {
+
+     static void Main(string[] args)
+     {
+            string Opener = @"            Lets build a creatue card!
+
+            I can create one randomly or you can make choices about it.
+            Please Select an option by typing the first word:";
+            string firstChoice = @"                    
+                    random creation
+            
+                    choose options";
+            Console.WriteLine(Opener);
+            
+            var loop = true;
+            
+            while(loop)
+            {
+            Console.WriteLine(firstChoice);
+            
+            var answer = Console.ReadLine();
+
+            if (answer == "random")
+
+            {
+                    Console.WriteLine("I'l create one now, one momemnt please.");
+                    break;
+                     
+            }
+
+                if (answer == "choose")
+                {
+                    Console.WriteLine("Okay let's see what options we have");
+                    break;
+
+                }
+
+                else
+                {
+                    Console.WriteLine("sorry I didn't get that, lets try again");
+                    
+                    loop = true;
+                }
+            }
+     }   
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        /*
+        public int Cost = 2;
+        public int Power = 2;
+        public int Toughness = 2;
+        public string CreatueType = "Dragon";
+        public string Rarity = "Mythic";
+        public string Abilites = "Flying Haste";
+       
+
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("This creature is a " + Rarity + " Dragon for 4RR with Flying Haste and has 6 and toughness 6");
+
+            
+        }
+      
+        
+      
+        /*  public int power = 0;
+        public int toughness = 0;
+        public string KeywordAbility = "Word"; 
+        public string Downside = "Bad Card Effects";
+        public string Upside = "Good Card Effects";
+        public string Rarity = "Determines the points available for the creature";
+        public int Cost= 0; //the mana value and color
+        public string Color = "The cards color"; //defines the keywords available
+        public string Legendary = "This is a random upside"; //more points and doesn't require downsides
+        public decimal? PointValue= 0; // will determine how many downsides/upsides
+        public string Type = "Dragon";
+
+        */
+        
+        
+
+            
+    }
+}
---
+++ Keyword.cs
@@ -0,0 +1,16 @@
+﻿namespace Creature
+{
+    public class Keyword
+    {
+        public Keyword()
+        {
+            
+            {
+                var random = new Random();
+                var list = new List<string> { "Trample", "Lifelink", "Vigilance", "Flying", "First Strike", "Double Strike", "Reach","Indestructible", "Flash", "Haste", "Defender", "Hexproof", "Menace", "Deathtouch", "Protection"};
+                int index = random.Next(list.Count);
+                Console.WriteLine(list[index]);
+            }
+        }
+    }
+}
